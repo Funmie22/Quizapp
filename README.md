3df69d3dfe0655afa146b39c6b1a050608435ae1
Dahunsi Funmilayo <funmi425@gmail.com>
2024-05-26 23:28:47 +0100

    A simple Quiz app
    Quiz app

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Quizapp
- This is a quiz app
+ This is a quiz app created using Html, Css and Javascript
+HTML Structure:
+Document Structure: The HTML document follows the standard structure with <!DOCTYPE html> declaration, <html>, <head>, and <body> tags.
+In the head, I have the title and I also linked the Css stylesheets for styling.
+CSS Styling:
+Global Styles: Resetting default margins, padding, and box-sizing.
+Layout Styling: Styling the header, navigation, main content, quiz sections, and popup info.
+JavaScript Functionality:
+Popup Info Interaction: JavaScript controls the behavior of the start button, exit button, and continue button for displaying instructions and starting the quiz.
+Dynamic Question Rendering: Questions and options are stored as an array of objects. JavaScript dynamically populates the quiz section with questions and options.
+Next Button Functionality: The next button allows users to navigate through questions sequentially.
+Event Handling: Event listeners are used to trigger actions like starting the quiz, exiting the popup, continuing to the quiz, and moving to the next question.
+Quiz Functionality:
+Question Data Structure: Each question is represented as an object containing properties like question text, options, correct answer, and question number.
+Question Rendering: JavaScript dynamically inserts question text and options into the HTML.
+Next Question Navigation: Users can progress to the next question by clicking the next button.
+
+Technical Improvements and Suggestions:
+Scoring Mechanism: Implement logic to calculate and display scores based on user responses.
+Answer Validation: Validate user-selected answers against the correct answer.
+Feedback: Provide feedback to users on correct and incorrect answers.
+Responsiveness: Enhance CSS for better responsiveness across different devices.
+Code Optimization: Refactor code for better readability, maintainability, and performance.
---
+++ question.js
@@ -0,0 +1,120 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does h mean in HTMl?",
+        answer: "A. heading",
+        options: [
+            "A. heading",
+            "B. title",
+            "C. sheet",
+            "D. Home"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Who is the head of Nigeria",
+        answer: "C. Tinubu Bola Ahmed",
+        options: [
+            "A. Tinubu Adebola",
+            "B. Ahmed Taye",
+            "C. Tinubu Bola Ahmed",
+            "D. Funmi Adebola"
+            ]
+    },
+
+    {
+        numb: 3,
+        question: "what is the odd one out of the options?",
+        answer: "B. Farming",
+        options: [
+            "A. HTML",
+            "B. Farming",
+            "C. CSS",
+            "D. Javascript"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What is the full meaning of Maths",
+        answer: "A. Mathematics",
+        options: [
+            "A. Mathematics",
+            "B. English",
+            "C. Statitics",
+            "D. Christain Religious"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What is the second name of Jasmine",
+        answer: "D. Omolara",
+        options: [
+            "A. Adebola",
+            "B. Chrissy",
+            "C. Violet",
+            "D. Omolara"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "which heading have the largest font size",
+        answer: "B. h1",
+        options: [
+            "A. h3",
+            "B. h1",
+            "C. h4",
+            "D. h5"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "what does CSS stands for?",
+        answer: "B. Cascading Stylesheet",
+        options: [
+            "A. Christy stylesite",
+            "B. Cascading Stylesheet",
+            "C. Cascading Styleplus",
+            "D. Christy Stylesheet"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "which is the odd one out?",
+        answer: "A. HTML",
+        options: [
+            "A. HTML",
+            "B. Java",
+            "C. Python",
+            "D. Javascript"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "Which of these delicacy belongs to Nigeria",
+        answer: "C. Pounded yam",
+        options: [
+            "A. Pizza",
+            "B. Pasta",
+            "C. Pounded yam",
+            "D. Chicken nuggets"
+
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "C in CSS stands for 'Cascading'",
+        answer: "A. True",
+        options: [
+            "A. True",
+            "B. false"
+        ]
+    },
+];
